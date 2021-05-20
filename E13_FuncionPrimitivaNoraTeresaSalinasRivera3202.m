d2b2da9452f3303b51e78af93787082836adf5a8
norasalinas <84082455+norasalinas@users.noreply.github.com>
2021-05-20 18:27:03 -0500

    Create E13_FuncionPrimitivaNoraTeresaSalinasRivera3202.m

---
+++ E13_FuncionPrimitivaNoraTeresaSalinasRivera3202.m
@@ -0,0 +1,22 @@
+% Title      :Funcion Primitiva
+% Description    :Script para graficar la actividad del tema funcion primitiva
+% Author    :Nora Teresa Salinas Rivera (Nora_16) familiasalinas6817@gmail.com
+% Date      :20210520
+% sion            :1
+% Usage      :octave> /path/FuncionPrimitiva_NoraTeresaSalinasRivera
+% Notes      :Requiere aplicacion octave usar en consola preferentemente.
+
+Definimos los intervalos de la funcion
+x=[0];
+Definimos la funcion
+y=(g);
+Imprimimos la funcion en grafica
+plot(x,y)
+Dibuja la cuadricula de la grafica
+grid on
+Continua graficando despues de una grafica existente
+hold on
+Grafica la funcion
+plot(x,y)
+Dibuja la grafica de barras sin espacios
+bar(x,y)
